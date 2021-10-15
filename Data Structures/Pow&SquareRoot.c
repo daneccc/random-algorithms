680de8a5dc60b3381afc07c31c6d58f94279f27c
Lucas Souza <lucassouzzamarques@gmail.com>
2021-10-15 00:24:07 -0300

    Pow & Square Root

---
+++ Data Structures/Pow&SquareRoot.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    float n = 4, k = 5;
+    printf("%f elevado a %f é igual %f\n\n", n, k, pow(4, 5));
+
+    printf("Raiz quadrada de 60 é: %f\n\n", sqrt(3600));
+}
